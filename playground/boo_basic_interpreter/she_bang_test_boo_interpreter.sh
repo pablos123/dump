9ead9568a6517f16a09f9c4520aa6a3e348a4284
Pablo <pablosaavedra123@gmail.com>
2025-07-07 22:17:17 -0300

    genc

---
+++ playground/boo_basic_interpreter/she_bang_test_boo_interpreter.sh
@@ -0,0 +1,46 @@
+#! boo
+
+    # This is a comment
+# This is also a comment
+
+
+# hello = [], bye = [1.0], declare = [1.0], 1 = ["declare"]
+coo blin
+            hello
+
+bye 1
+
+declare 1
+
+1 declare
+
+# Print values
+poo hello bye declare 1
+
+# declare = []
+declare
+
+# Print values, spaces do not matter
+poo      hello    bye declare    1
+
+# Concatenate declare and bye, store it in declare
+coo declare bye
+
+# Add four zeros to declare
+coo declare 0 0 0 0
+
+poo declare:::: declare
+
+# Do nothing
+goo
+
+# Execute the line 43 as the next instruction
+goo 43
+
+poo this is not going to be printed
+
+# End the program
+endboo
+
+poo nothing here is going to be executed
+
