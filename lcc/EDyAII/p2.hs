22ee700afb6d2104f49487ea4357fb369598baaa
Pablo <pablosaavedra123@gmail.com>
2026-04-15 19:12:20 -0300

    genc

---
+++ lcc/EDyAII/p2.hs
@@ -0,0 +1,15 @@
+module Lab01 where
+
+import Data.List
+
+not b = case b of
+    True -> False
+    False -> True
+
+-- in [x] = []
+-- in x:xs = x : in xs
+
+main = do
+    putStrLn $ show $  2
+
+
