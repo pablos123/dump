8a44131b4e67d60231479e522e580c2848d1783d
Pablo <pablosaavedra123@gmail.com>
2026-04-15 23:28:45 -0300

    genc

--- lcc/EDyAII/labs/lab.hs
+++ lcc/EDyAII/labs/lab.hs
@@ -1,13 +1,13 @@
-import Data.List
-
-data CList a = EmptyCL
-                | CUnit a
-                | Consnoc a (CList a) a deriving Show
+data CList a
+  = EmptyCL
+  | CUnit a
+  | Consnoc a (CList a) a
+  deriving (Show)
 
 -- EmptyCL :: CList a
 -- CUnit :: a -> cList a
 -- Cansnoc :: a -> CList a -> a -> CList a
---
+
 appendElem :: CList a -> a -> CList a
 appendElem EmptyCL x = CUnit x
 appendElem (CUnit y) x = Consnoc y EmptyCL x
@@ -22,19 +22,18 @@ inits :: CList a -> CList (CList a)
 inits EmptyCL = CUnit EmptyCL
 inits (CUnit x) = Consnoc EmptyCL EmptyCL (CUnit x)
 inits c@(Consnoc x l y) = Consnoc EmptyCL m c
-                            where m = mapPrepend (inits l) x
+  where
+    m = mapPrepend (inits l) x
 
-mapPrepend :: Clist (Clist a) -> a -> CList (Clist a)
+mapPrepend :: CList (CList a) -> a -> CList (CList a)
 mapPrepend EmptyCL x = EmptyCL
 mapPrepend (CUnit l) x = CUnit (prepend x l)
-mapPrepend (Consnoc l1 ls l2) x = Consnoc (prepend x l1) (mapPrepend l2 x ) (consCl x l2)
-
+mapPrepend (Consnoc l1 ls l2) x = Consnoc (prepend x l1) (mapPrepend l2 x) (consCl x l2)
 
 prepend :: a -> CList a -> CList a
 prepend x EmptyCL = CUnit x
 prepend x (CUnit y) = prepend x EmptyCl y
 prepend x (Consnoc a l b) = Consnoc x (prepend x l) b
-
 
 tailCL :: CList a -> CList a
 tailCL (CUnit x) = EmptyCL
@@ -44,4 +43,4 @@ test :: Int -> Char
 test x = 'a'
 
 main = do
-    putStrLn $ show $  test 5
+  print (test 5)
