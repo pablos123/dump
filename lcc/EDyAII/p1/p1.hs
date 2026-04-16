d76884bca1d3b7f3d32ed6b9fbdc0997720070a5
Pablo <pablosaavedra123@gmail.com>
2026-04-16 17:30:17 -0300

    genc

--- lcc/EDyAII/p1/p1.hs
+++ lcc/EDyAII/p1/p1.hs
@@ -1,60 +1,52 @@
-import Data.List
 import Data.Char
+import Data.List
 
 -- Ejercicios de teoría.
-is_even :: Int -> Bool
-is_even 0 = True
-is_even x = mod x 2 == 0
+isEven :: Int -> Bool
+isEven 0 = True
+isEven x = even x -- mod x 2 == 0
 
 takeWhile1 :: (a -> Bool) -> [a] -> [a]
 takeWhile1 p [] = []
-takeWhile1 p (x:xs) | p x = x:takeWhile1 p xs
-                    | otherwise = []
+takeWhile1 p (x : xs)
+  | p x = x : takeWhile1 p xs
+  | otherwise = []
 
 dropWhile1 :: (a -> Bool) -> [a] -> [a]
 dropWhile1 p [] = []
-dropWhile1 p (x:xs)  | p x = dropWhile1 p xs
-                     | otherwise = x:xs
+dropWhile1 p (x : xs)
+  | p x = dropWhile1 p xs
+  | otherwise = x : xs
 
 span1 :: (a -> Bool) -> [a] -> ([a], [a])
 span1 p [] = ([], [])
-span1 p (x) = (takeWhile1 p x, dropWhile1 p x)
+span1 p x = (takeWhile1 p x, dropWhile1 p x)
 
 span2 :: (a -> Bool) -> [a] -> ([a], [a])
 span2 p [] = ([], [])
-span2 p (x:xs) | p x = (x:y, z)
-               | otherwise =  ([], x:xs)
-               where (y, z) = span2 p xs
+span2 p (x : xs)
+  | p x = (x : y, z)
+  | otherwise = ([], x : xs)
+  where
+    (y, z) = span2 p xs
 
 -- Práctica 1
-sum_two :: Num a => a -> a
-sum_two x = x + 2
+sumTwo :: (Num a) => a -> a
+sumTwo x = x + 2
 
-sum_three :: Num a => a -> a
-sum_three x = x + 3
+sumThree :: (Num a) => a -> a
+sumThree x = x + 3
 
 test :: (Num a, Eq a) => (a -> a) -> a -> Bool
 test f x = f x == x + 2
 
-showme :: Show a => a -> String
-showme x = show x
-
-testing :: a -> (b -> b)
-testing x = \x -> x
-
-
 main = do
-    -- Teoría
-    let list = [2,1,2,3,4]
-    putStrLn $ show $ takeWhile1 is_even list
-    putStrLn $ show $ dropWhile1 is_even list
-    putStrLn $ show $ span1 is_even list
-    putStrLn $ show $ span2 is_even list
+  -- Teoría
+  let list = [2, 1, 2, 3, 4]
+  print (takeWhile1 isEven list)
+  print (dropWhile1 isEven list)
+  print (span1 isEven list)
+  print (span2 isEven list)
 
-    -- Práctica 1
-    putStrLn $ show $ test sum_two 2
-    putStrLn $ show $ testing 'a' 2
-    putStrLn $ show $ (\x -> x + 1) 3
-
-    putStrLn $ show $ zip [1,2] [3,4]
-
+  print ()
+  print ((+ 1) 3)
