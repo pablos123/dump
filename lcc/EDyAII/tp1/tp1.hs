f5ee39db35fcd49cc1b62087ea80c88179eab944
Pablo <pablosaavedra123@gmail.com>
2026-04-28 19:44:44 -0300

    genc

--- lcc/EDyAII/tp1/tp1.hs
+++ lcc/EDyAII/tp1/tp1.hs
@@ -1,3 +1,5 @@
+import Data.List (sortBy)
+
 data NdTree p
   = Node
       (NdTree p) -- sub´arbol izquierdo
@@ -35,10 +37,25 @@ instance Punto Punto3d where
   coord 2 (P3d (_, _, k)) = k
   coord _ (P3d _) = error "Bad index for Punto3d"
 
+fromList :: (Punto p) => [p] -> NdTree p
+fromList [] = Empty
+fromList xs = fromList' xs 0 (dimension (head xs))
+
+fromList' :: (Punto p) => [p] -> Int -> Int -> NdTree p
+fromList' [] _ _ = Empty
+fromList' xs i d =
+  let eje = i `mod` d
+      sorted = sortBy (\x y -> compare (coord eje x) (coord eje y)) xs
+      m_ind = length sorted `div` 2
+   in Node (fromList' (take m_ind sorted) (i + 1) d) (sorted !! m_ind) (fromList' (drop (m_ind + 1) sorted) (i + 1) d) eje
+
 main :: IO ()
 main = do
   let test_p2d = P2d (1, 2)
   let test_p3d = P3d (1, 2, 3)
+  let test_list_p2d = [P2d (2, 3), P2d (5, 4), P2d (9, 6), P2d (4, 7), P2d (8, 1), P2d (7, 2)]
+  let test_list_p2d_1 = [P2d (2, 3), P2d (5, 4), P2d (9, 6)]
+  let test_list_p2d_2 = [P2d (5, 4), P2d (2, 3), P2d (9, 6)]
 
   print test_p2d
   print (coord 1 test_p2d)
@@ -49,3 +66,7 @@ main = do
   print (coord 2 test_p3d)
   print (dimension test_p3d)
   print (dist test_p3d test_p3d)
+
+  print (fromList test_list_p2d)
+  print (fromList test_list_p2d_1)
+  print (fromList test_list_p2d_2)
